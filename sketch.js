3a10b3768ce763f515c55b509cdfafe5bee0e355
kirbyyourmom <34996878+kirbyyourmom@users.noreply.github.com>
2021-05-03 18:58:57 +0100

    Formatted code.

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ const NO_NODES = 0;
 function setup() {
   createCanvas(600, 600);
   ellipseMode(RADIUS);
-  
+
   for (let i = 0; i < nodes; i++) {
     circles.push(new Circle(i));
   }
@@ -21,24 +21,29 @@ function draw() {
   stroke(50);
   ellipse(0, 0, circleRadius, circleRadius);
   frameRate(60);
-  
+
   for (let i of circles) {
     i.show(NO_NODES);
-    line(i.x, i.y, circles[floor(i.i * multiple % nodes)].x, circles[floor(i.i * multiple % nodes)].y);
+    line(
+      i.x,
+      i.y,
+      circles[floor((i.i * multiple) % nodes)].x,
+      circles[floor((i.i * multiple) % nodes)].y
+    );
   }
   multiple += 0.01;
 }
 
 function Circle(i) {
   this.i = i;
-  this.x = circleRadius * cos(PI - (TAU / nodes * this.i));
-  this.y = circleRadius * sin(PI - (TAU / nodes * this.i));
-  
+  this.x = circleRadius * cos(PI - (TAU / nodes) * this.i);
+  this.y = circleRadius * sin(PI - (TAU / nodes) * this.i);
+
   this.show = (mode) => {
     if (mode == 1) {
       noFill();
       stroke(50);
       circle(this.x, this.y, 1);
     }
-  }
+  };
 }
