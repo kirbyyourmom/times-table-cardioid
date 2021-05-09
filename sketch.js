7041be850f078a1fbcc495ed4f222bf1b784764e
kirbyyourmom <34996878+kirbyyourmom@users.noreply.github.com>
2021-05-09 09:12:56 +0100

    Dark mode, removed debugging code, made look nice.

--- sketch.js
+++ sketch.js
@@ -2,48 +2,37 @@ let circles = [];
 let nodes = 150;
 let multiple = 0.01;
 let circleRadius = 250;
-const SHOW_NODES = 1;
-const NO_NODES = 0;
 
 function setup() {
-  createCanvas(600, 600);
-  ellipseMode(RADIUS);
+	createCanvas(600, 600);
+	ellipseMode(RADIUS);
 
-  for (let i = 0; i < nodes; i++) {
-    circles.push(new Circle(i));
-  }
+	for (let i = 0; i < nodes; i++) {
+		circles.push(new Circle(i));
+	}
 }
 
 function draw() {
-  background(255);
-  translate(width / 2, height / 2);
-  noFill();
-  stroke(50);
-  ellipse(0, 0, circleRadius, circleRadius);
-  frameRate(60);
+	background(21);
+	translate(width / 2, height / 2);
+	noFill();
+	stroke(255);
+	ellipse(0, 0, circleRadius, circleRadius);
+	frameRate(60);
 
-  for (let i of circles) {
-    i.show(NO_NODES);
-    line(
-      i.x,
-      i.y,
-      circles[floor((i.i * multiple) % nodes)].x,
-      circles[floor((i.i * multiple) % nodes)].y
-    );
-  }
-  multiple += 0.01;
+	for (let i of circles) {
+		line(
+			i.x,
+			i.y,
+			circles[floor((i.i * multiple) % nodes)].x,
+			circles[floor((i.i * multiple) % nodes)].y
+		);
+	}
+	multiple += 0.01;
 }
 
 function Circle(i) {
-  this.i = i;
-  this.x = circleRadius * cos(PI - (TAU / nodes) * this.i);
-  this.y = circleRadius * sin(PI - (TAU / nodes) * this.i);
-
-  this.show = (mode) => {
-    if (mode == 1) {
-      noFill();
-      stroke(50);
-      circle(this.x, this.y, 1);
-    }
-  };
+	this.i = i;
+	this.x = circleRadius * cos(PI - (TAU / nodes) * this.i);
+	this.y = circleRadius * sin(PI - (TAU / nodes) * this.i);
 }
